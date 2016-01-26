fd047d290308839a0d85fe7c7bc7a62f09567849
DiasNurdicanov <dison08@mail.ru>
2016-01-26 11:25:23 +0600

    Решение с новыми методами

--- js/check.js
+++ js/check.js
@@ -1,39 +1,39 @@
 function getMessage(a,b) {
+	//Проверка выстрела
 	if ( typeof a == 'boolean' ){
-
 		if ( a ){
 			return 'Я попал в ' + b;
 		} else{
 			return 'Я никуда не попал';
 		};
 	}
-	else if ( typeof a == 'number' ){
+
+	//Проверка прыжка
+	if ( typeof a == 'number' ){
 		return 'Я прыгнул на ' + (a * 100) + ' сантиметров';
 	}
-	else if ( (typeof a == "object") && (a instanceof Array) ){
-		console.log(a);
+	
+	//Проверка достижения правого края
+	if ( (Array.isArray(a)) && !(Array.isArray(b)) ){
 
-		var sum = 0;
+		var sum = a.reduce(function(result, current) {
+		  return result + current;
+		},0);
 
-		for (var i = 0; i < a.length; i++) {
-			sum += a[i];
-		};
-
-		console.log(sum);
-
-		return 'Я прошел ' + sum + ' шагов';
+		return 'Я прошёл ' + sum + ' шагов';
 	}
-	else if ( (typeof a == "object") && (a instanceof Array) && (typeof b == "object") && (b instanceof Array) ){
+	
+	//Проверка достижения левого края
+	//Тут количество элементов должно быть одинаковым
+	if ( Array.isArray(a) && Array.isArray(b) ){
 
-		var length = 0;
+		var length = a.reduce(function(result, current, i) {
+		  var sum = current + b[i];
 
-		for (var i = 0; i < a.length; i++) {
-			var sum = a[i] + b[i];
+		  return result + sum;
 
-			length += sum;
-		};
+		},0)
 
-		
-		return 'Я прошел ' + length + ' метров';
+		return 'Я прошёл ' + length + ' метров';
 	}
 }
